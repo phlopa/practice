feccd92cf89dba5e7a88378b6d828e3b23506f8b
phlopa <203694213+phlopa@users.noreply.github.com>
2026-01-29 21:02:44 +0100

    feat: add campaign detail pages, charts integration and img widget

--- js/app/pages/campaigns.js
+++ js/app/pages/campaigns.js
@@ -3,12 +3,13 @@ export const campaigns = {
         return {
             parent: "",
             details:{},
-            data: { items: [] },
+            data: {},
             date:"",
             date2:"",
             q:"",
             sort:"",
             loader: 1,
+            iChart:-1,
             id:0,
             type:0
         };
@@ -39,6 +40,7 @@ export const campaigns = {
             if(this.date!="") data.append('date',this.date);
             if(this.date2!="") data.append('date2',this.date2);
             */
+            if(self.iChart!=-1) self.line(self.data.items[self.iChart]);
             self.loader=1;
             axios.post(this.parent.url+"/site/getCampaigns?auth="+this.parent.user.auth, data).then(function(response){
                 self.data = response.data;
@@ -54,7 +56,9 @@ export const campaigns = {
 
             axios.post(this.parent.url+"/site/actionCampaign?auth="+this.parent.user.auth, data).then(function(response){
                 self.$refs.new.active=0;
-                if(self.parent.formData.id){
+                if(response.data.error){
+                    self.$refs.header.$refs.msg.alertFun(response.data.error);
+                } else if(self.parent.formData.id){
                     self.$refs.header.$refs.msg.successFun("Successfully updated campaign!");
                 }
                 else{
@@ -83,6 +87,108 @@ export const campaigns = {
                     console.log('errors: ', error);
                 });
             }
+        },
+        line: function(item){
+            setTimeout(function(){
+                let dates = [];
+                let clicks = [];
+                let views = [];
+                let leads = [];
+                if(item&&item['line']){
+                    for(let i in item['line']){
+                        dates.push(i);
+                        clicks.push(item['line'][i].clicks);
+                        views.push(item['line'][i].views);
+                        leads.push(item['line'][i].leads);
+                    }
+                }
+
+                document.getElementById('chartOuter').innerHTML =
+                `
+                    <div id="chartHints">
+                        <div class="chartHintsViews">Views</div>
+                        <div class="chartHintsClicks">Clicks</div>
+                    </div>
+                    <canvas id="myChart"></canvas>
+                `
+                const ctx = document.getElementById('myChart');
+                const xScaleImage ={
+                    id:'xScaleImage',
+                    afterDatasetsDraw(chart, args, plugins){
+                        const {ctx, data, chartArea:{bottom}, scales:{x}} = chart;
+                        ctx.save();
+                        data.images.forEach((image, index) => {
+                            const label = new Image();
+                            label.src=image;
+
+                            const width = 120;
+                            src.drawImage(label, x.getPixelForValue(index)-(width/2), x.top, width, width);
+
+                        });
+                    }
+                }
+                new Chart(ctx, {
+                    type:'line',
+                    data:{
+                        labels: dates,
+                        datasets:[
+                            {
+                                label:"Clicks",
+                                backgroundColor:"#00599D",
+                                borderColor:"#00599D",
+                                data: clicks
+                            },
+                            {
+                                label:"Views",
+                                backgroundColor:"#5000B8",
+                                borderColor:"#5000B8",
+                                data: views,
+                                yAxisID: 'y2'
+                            },
+                        ]
+                    },
+                    options:{
+                        responsive:true,
+                        plugins:{
+                            tooltip:{
+                                bodyFontSize:20,
+                                usePointStyle:true,
+                                callbacks:{
+                                    title:(ctx)=>{
+                                        return ctx[0]['dataset'].label
+                                    },
+                                }
+                            },
+                            legend:{
+                                display:false
+                            }
+                        },
+                        categoryPercentage:0.2,
+                        barPercentage:0.8,
+                        scales:{
+                            y:{
+                                id:'y2',
+                                position:'right'
+                            },
+                            x:{
+                                afterFit: (scale)=>{
+                                    scale.height=120;
+                                }
+                            }
+                        }
+                    },
+
+                })
+            },1000);
+        },
+        checkAll:function(prop){
+            if(this.data.items[this.iChart].sites){
+                for(let i in this.data.items[this.iChart].sites){
+                    this.data.items[this.iChart].sites[i].include=prop;
+                }
+            }
+            this.parent.formData=this.data.items[this.iChart];
+            this.get();
         }
     },
     template: `
@@ -105,6 +211,61 @@ export const campaigns = {
                         <a class="btnS" href="#" @click.prevent="parent.formData={};$refs.new.active=1"><i class="fas fa-plus"></i>  New </a>
                     </div>
                 </div>
+
+                <popup ref="chart" fullscreen="true" title="Chart">
+                    <div class="flex panel">
+                        <div class="w30 ptb25">
+                            <input type="date" v-model="date" @change="get();" /> – <input type="date" v-model="date2" @change="get();" />
+                        </div>
+
+                        <div class="w70 al">
+                            <div class="flex cubes">
+                                <div class="w30 clicks">
+                                    <div>Clicks</div>
+                                    {{data.items[iChart].clicks}}
+                                </div>
+                                
+                                <div class="w30 views">
+                                    <div>Views</div>
+                                    {{data.items[iChart].views}}
+                                </div>
+                                
+                                <div class="w30 leads">
+                                    <div>Leads</div>
+                                    {{data.items[iChart].leads}}
+                                </div>
+                                
+                                <div class="w30 ctr">
+                                    <div>CTR</div>
+                                    {{((data.items[iChart].clicks * 100 / data.items[iChart].views).toFixed(2))}} %
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex body">
+                        <div class="w30 ar filchart">
+                            <div class="itemchart ptb10" v-if="all">
+                                <toogle v-model="all" @update:modelValue="all = $event; checkAll($event)" />
+                                All
+                            </div>
+                            
+                            <div class="itemchart ptb10" v-if="data.items[iChart].sites" v-for="s in data.items[iChart].sites">
+                                <toogle v-model="s.include" @update:modelValue="s.include = $event; parent.formData = data.items[iChart]; get()" />
+                                {{s.site}}
+                            </div>
+                        </div>
+
+                        <div class="w70" id="chartOuter">
+                            <div id="chartHints">
+                                <div class="chartHintsViews">Views</div>
+                                <div class="chartHintsClicks">Clicks</div>
+                            </div>
+                            <canvas id="myChart"></canvas>
+                        </div>
+                    </div>
+                </popup>
+
                 <popup ref="new" :title="(parent.formData && parent.formData.id) ? 'Edit campaign' : 'New campaign'">
                     <div class="form inner-form">
                         <form @submit.prevent="action()" v-if="parent.formData">
@@ -167,6 +328,12 @@ export const campaigns = {
                                     </a>
                                 </td>
                                 <td class="actions">
+                                    <router-link :to="'/campaign/'+item.id">
+                                        <i class="fas fa-edit"></i>
+                                    </router-link>
+                                    <a href="#" @click.prevent="parent.formData = item; iChart=i; $refs.chart.active=1; line(item)">
+                                        <i class="fas fa-chart-bar"></i>
+                                    </a>
                                     <a href="#" @click.prevent="parent.formData = item; del();">
                                         <i class="fas fa-trash-alt"></i>
                                     </a>
--- js/app/router.js
+++ js/app/router.js
@@ -1,6 +1,6 @@
 import {login} from './pages/login.js';
-import {campaigns} from './pages/campaigns.js';/*
-import {campaign} from './pages/campaign.js';
+import {campaigns} from './pages/campaigns.js';
+import {campaign} from './pages/campaign.js';/*
 import {users} from './pages/users.js';
 import {user} from './pages/user.js';
 
@@ -13,8 +13,8 @@ export const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes: [
         { path: '/', name: 'Sign in', component: login },
-        { path: '/campaigns', name: 'Campaigns', component: campaigns }/*,
-        { path: '/campaign/:id', name: 'Campaign', component: campaign },
+        { path: '/campaigns', name: 'Campaigns', component: campaigns },
+        { path: '/campaign/:id', name: 'Campaign', component: campaign }/*,
         { path: '/users', name: 'Users', component: users },
         { path: '/user/:id', name: 'User', component: user },
         { path: '/ads', name: 'Ads', component: ads },
