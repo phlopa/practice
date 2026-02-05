4fe4d3648b9bb52239cd1787a523a9c493cd2eaf
phlopa <203694213+phlopa@users.noreply.github.com>
2026-02-05 13:01:52 +0100

    feat: add mobile responsive layout

--- js/app/widgets/header.js
+++ js/app/widgets/header.js
@@ -32,7 +32,7 @@ export const header = {
                     </div>
                     <div class="w70">
                         <div id="menu">
-                            <!--<i class="fas fa-bars" @click="menu=1"></i>-->
+                            <i class="fas fa-bars" @click="menu=1"></i>
 
                             <ul :class="{active:menu==1}" v-if="parent.user && parent.user.type && parent.user.type=='admin'">
                                 <li v-if="menu==1" class="al"><i class="fas fa-times" @click="menu=0"></i></li>
